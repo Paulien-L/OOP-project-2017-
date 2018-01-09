6fc90f28f77baad5b0d91aa64453333c600a5ee6
Paulien-L <34313853+Paulien-L@users.noreply.github.com>
2018-01-09 15:36:23 +0100

    Update Project_monstertest.java

--- Project_monstertest.java
+++ Project_monstertest.java
@@ -1,223 +1,294 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-class MonsterTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+public class Testing {
 	
-	private Monster monsterLegal, monsterAverage, monsterOverPowered;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		monsterLegal = new Monster("Grog", 5, 5, 10, 10);
-		monsterAverage = new Monster("Greg", 5, 5, 10, 10);
-		monsterOverPowered = new Monster("Demonlord", 5, 5, 10, 100);
+	private Monster monsterLegal;
+	private Weapon weaponLegal;
+	private Purse purseLegal;
+	private Backpack backpackLegal;
+
+	@Before
+	public void setUp() throws Exception {
+		weaponLegal = new Weapon(10, 5, 5, null);
+		purseLegal = new Purse(111, 5, 10, null, 10, 0);
+		backpackLegal = new Backpack(110, 5, 5, 5, null);
+		
+		monsterLegal = new Monster("Grog", 5, 5, 10, 100, 4, weaponLegal, purseLegal, backpackLegal);
 	}
 	
 	//CONSTRUCTOR
-	@Test
-	public final void constructor_LegalCase() {
-		Monster newMonster = new Monster("Azrael", 10, 11, 10, 20);
-		assertEquals("Azrael", newMonster.getName());
-		assertEquals(10, newMonster.getDamage());
-		assertEquals(11, newMonster.getProtection());
-		assertEquals(10, newMonster.getMaxHitpoints());
-		assertEquals(20, newMonster.getStrength());
-	}
-	
-	@Test
-	public final void constructor_Throws() {
-	assertThrows(IllegalArgumentException.class, () -> {
-		Monster myMonster = new Monster("@zra3l", 10, 11, 10, 20);
-	});
-	assertThrows(IllegalArgumentException.class, () -> {
-		Monster yourMonster = new Monster("Azrael", 10, 11, -44, 20);
-	});
-	}
+    @Test
+    public final void constructor_LegalCase() {
+        assertEquals("Grog", monsterLegal.getName());
+        assertEquals(5, monsterLegal.getDamage());
+        assertEquals(5, monsterLegal.getProtection());
+        assertEquals(10, monsterLegal.getMaxHitpoints());
+        assertEquals(100, monsterLegal.getStrength());
+        assertEquals(4, monsterLegal.getNbAnchors());
+        assertTrue(monsterLegal.hasAsEquipment(weaponLegal));
+        assertTrue(monsterLegal.hasAsEquipment(purseLegal));
+        assertTrue(monsterLegal.hasAsEquipment(backpackLegal));
+    }
+    
+    @Test
+    public final void constructor_Throws() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Monster myMonster = new Monster("azrael", 10, 11, 10, 20 ,3, weaponLegal, purseLegal, backpackLegal);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Monster yourMonster = new Monster("Azrael", 10, 11, -44, 20, 3, weaponLegal, purseLegal, backpackLegal);
+        });
+        
+        assertThrows(IllegalArgumentException.class, () -> {
+            Monster hisMonster = new Monster("Azrael", 10, 11, 10, 20, -3, weaponLegal, purseLegal, backpackLegal);
+        });
+    }
+    
+    //NAME
+    @Test
+    public final void getName_SingleCase() {
+        assertEquals("Grog", monsterLegal.getName());
+    }
+    
+    @Test
+    public final void isValidName_TrueCase() {
+        assertTrue(monsterLegal.isValidName("Grog"));
+    }
+    
+    @Test
+    public final void isValidName_FalseCase() {
+        assertFalse(monsterLegal.isValidName("Grog2.0"));
+    }
+    
+    //DAMAGE
+    @Test
+    public final void getDamage_SingleCase() {
+        assertEquals(5, monsterLegal.getDamage());
+    }
+    
+    @Test
+    public final void setDamage_OverMaximum() {
+        monsterLegal.setDamage(40);
+        assertEquals(20, monsterLegal.getDamage());
+    }
 
-	//NAME
-	@Test
-	public final void getName_SingleCase() {
-		assertEquals("Grog", monsterLegal.getName());
-	}
-	
-	@Test
-	public final void isValidName_TrueCase() {
-		assertTrue(monsterLegal.isValidName("Grog"));
-	}
-	
-	@Test
-	public final void isValidName_FalseCase() {
-		assertFalse(monsterLegal.isValidName("Grog2.0"));
-	}
-	
-	//DAMAGE
-	@Test
-	public final void getDamage_SingleCase() {
-		assertEquals(5, monsterLegal.getDamage());
-	}
-	//This one also tests the a legal case for setDamage()
-	
-	@Test
-	public final void setDamage_OverMaximum() {
-		monsterLegal.setDamage(40);
-		assertEquals(20, monsterLegal.getDamage());
-	}
-	
-	@Test
-	public final void setDamage_UnderMinimum() {
-		monsterLegal.setDamage(-20);
-		assertEquals(1, monsterLegal.getDamage());
-	}
-	
-	@Test
-	public final void getMaxDamage_SingleCase() {
-		assertEquals(20, monsterLegal.getMaxDamage());
-	}
-	
-	@Test
-	public final void setMaxDamage_LegalCase() {
-		monsterLegal.setMaxDamage(40);
-		assertEquals(40, monsterLegal.getMaxDamage());
-	}
-	
-	@Test
-	public final void setMaxDamage_IllegalCase() {
-		monsterLegal.setMaxDamage(-4);
-		assertEquals(20, monsterLegal.getMaxDamage());
-	}
-	
-	//PROTECTION
-	@Test
-	public final void getProtection_SingleCase() {
-		assertEquals(5, monsterLegal.getProtection());
-	}
-	
-	@Test
-	public final void isValidProtection_TrueCase() {
-		assertTrue(monsterLegal.isValidProtection(11));
-	}
-	
-	@Test
-	public final void isValidProtection_FalseCase() {
-		assertFalse(monsterLegal.isValidProtection(-24)); //Less than minimum protection
-		assertFalse(monsterLegal.isValidProtection(42)); //More than max protection
-		assertFalse(monsterLegal.isValidProtection(4)); //Not a prime
-	}
-	
-	@Test
-	public final void getMaxProtection_SingleCase() {
-		assertEquals(40, monsterLegal.getMaxProtection());
-	}
-	
-	@Test
-	public final void isValidMaxProtection_TrueCase() {
-		assertTrue(monsterLegal.isValidMaxProtection(11));
-	}
-	
-	@Test
-	public final void isValidMaxProtection_FalseCase() {
-		assertFalse(monsterLegal.isValidMaxProtection(-20)); //Less than minimum protection
-		assertFalse(monsterLegal.isValidMaxProtection(8)); //Not a prime
-	}
-	
-	@Test
-	public final void setMaxProtection_LegalCase() {
-		monsterLegal.setMaxProtection(11);
-		assertEquals(11, monsterLegal.getMaxProtection());
-	}
-	
-	//HITPOINTS
-	@Test
-	public final void getHitpoints_SingleCase() {
-		monsterLegal.setHitpoints(9);
-		assertEquals(9, monsterLegal.getHitpoints());
-	}
-	//Also tests legal case for setHitpoints()
-	
-	@Test
-	public final void isValidHitpoints_TrueCase() {
-		assertTrue(monsterLegal.isValidHitpoints(9));
-	}
-	
-	@Test
-	public final void isValidHitpoints_FalseCase() {
-		assertFalse(monsterLegal.isValidHitpoints(20)); //More than max hitpoints
-		assertFalse(monsterLegal.isValidHitpoints(-5)); //Less than 0
-	}
-	
-	@Test 
-	public final void setHitpoints_IllegalCase() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> monsterLegal.setHitpoints(-4));
-	}
-	
-	@Test
-	public final void getMaxHitpoints_SingleCase() {
-		assertEquals(10, monsterLegal.getMaxHitpoints());
-	}
-	
-	@Test
-	public final void isValidMaxHitpoints_TrueCase() {
-		assertTrue(monsterLegal.isValidMaxHitpoints(20));
-	}
-	
-	@Test
-	public final void isValidMaxHitpoints_FalseCase() {
-		assertFalse(monsterLegal.isValidMaxHitpoints(0));
-	}
-	
-	@Test
-	public final void setMaxHitpoints_LegalCase() {
-		monsterLegal.setMaxHitpoints(20);
-		assertEquals(20, monsterLegal.getMaxHitpoints());
-	}
-	
-	@Test
-	public final void setMaxHitpoints_IllegalCase() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> monsterLegal.setMaxHitpoints(-4));
-	}
-	
-	@Test
-	public final void hasDied_TrueCase() {
-		monsterLegal.setHitpoints(0);
-		assertTrue(monsterLegal.hasDied());
-	}
-	
-	@Test
-	public final void hasDied_FalseCase() {
-		monsterLegal.setHitpoints(5);
-		assertFalse(monsterLegal.hasDied());
-	}
-	
-	@Test
-	public final void getStrength_SingleCase() {
-		assertEquals(10, monsterLegal.getStrength());
-	}
-	
-	@Test
-	public final void getAverageStrength_SingleCase() {
-		assertEquals(10, Monster.getAverageStrength(monsterLegal.getStrength(), monsterAverage.getStrength()));
-	}
-	
-	@Test
-	public final void isValidAverageStrength_TrueCase() {
-		float average = Monster.getAverageStrength(monsterLegal.getStrength(), monsterAverage.getStrength());
-		assertTrue(Monster.isValidAverageStrength(average));
-	}
-	
-	@Test
-	public final void isValidAverageStrength_FalseCase() {
-		float average = Monster.getAverageStrength(monsterLegal.getStrength(), monsterOverPowered.getStrength());
-		assertFalse(Monster.isValidAverageStrength(average));
-	}
+    @Test
+    public final void setDamage_UnderMinimum() {
+        monsterLegal.setDamage(-20);
+        assertEquals(1, monsterLegal.getDamage());
+    }
+
+    @Test
+    public final void getMaxDamage_SingleCase() {
+        assertEquals(20, monsterLegal.getMaxDamage());
+    }
+
+    @Test
+    public final void setMaxDamage_LegalCase() {
+        monsterLegal.setMaxDamage(40);
+        assertEquals(40, monsterLegal.getMaxDamage());
+    }
+
+    @Test
+    public final void setMaxDamage_IllegalCase() {
+        monsterLegal.setMaxDamage(-4);
+        assertEquals(20, monsterLegal.getMaxDamage());
+    }
+    
+  //PROTECTION
+    @Test
+    public final void getProtection_SingleCase() {
+        assertEquals(5, monsterLegal.getProtection());
+    }
+
+    @Test
+    public final void isValidProtection_TrueCase() {
+        assertTrue(monsterLegal.isValidProtection(11));
+    }
+
+    @Test
+    public final void isValidProtection_FalseCase() {
+        assertFalse(monsterLegal.isValidProtection(-24)); //Less than minimum protection
+        assertFalse(monsterLegal.isValidProtection(42)); //More than max protection
+        assertFalse(monsterLegal.isValidProtection(4)); //Not a prime
+    }
+
+    @Test
+    public final void getMaxProtection_SingleCase() {
+        assertEquals(40, monsterLegal.getMaxProtection());
+    }
+
+    @Test
+    public final void isValidMaxProtection_TrueCase() {
+        assertTrue(monsterLegal.isValidMaxProtection(11));
+    }
+
+    @Test
+    public final void isValidMaxProtection_FalseCase() {
+        assertFalse(monsterLegal.isValidMaxProtection(-20)); //Less than minimum protection
+        assertFalse(monsterLegal.isValidMaxProtection(8)); //Not a prime
+    }
+
+    @Test
+    public final void setMaxProtection_LegalCase() {
+        monsterLegal.setMaxProtection(11);
+        assertEquals(11, monsterLegal.getMaxProtection());
+    }
+    
+    //HITPOINTS
+    @Test
+    public final void getHitpoints_SingleCase() {
+        assertEquals(10, monsterLegal.getHitpoints());
+    }
+
+    @Test
+    public final void isValidHitpoints_TrueCase() {
+        assertTrue(monsterLegal.isValidHitpoints(9));
+    }
+
+    @Test
+    public final void isValidHitpoints_FalseCase() {
+        assertFalse(monsterLegal.isValidHitpoints(20)); //More than max hitpoints
+        assertFalse(monsterLegal.isValidHitpoints(-5)); //Less than 0
+    }
+
+    @Test
+    public final void getMaxHitpoints_SingleCase() {
+        assertEquals(10, monsterLegal.getMaxHitpoints());
+    }
+
+    @Test
+    public final void isValidMaxHitpoints_TrueCase() {
+        assertTrue(monsterLegal.isValidMaxHitpoints(20));
+    }
+
+    @Test
+    public final void isValidMaxHitpoints_FalseCase() {
+        assertFalse(monsterLegal.isValidMaxHitpoints(0));
+    }
+
+    @Test
+    public final void setMaxHitpoints_LegalCase() {
+        monsterLegal.setMaxHitpoints(20);
+        assertEquals(20, monsterLegal.getMaxHitpoints());
+    }
+
+    @Test
+    public final void setMaxHitpoints_IllegalCase() {
+        assertThrows(IllegalArgumentException.class, () -> monsterLegal.setMaxHitpoints(-4));
+    }
+    
+    @Test
+    public final void hasDied_FalseCase() {
+        assertFalse(monsterLegal.hasDied());
+    }
+    
+    //STRENGTH
+    @Test
+    public final void getStrength_SingleCase() {
+        assertEquals(100, monsterLegal.getStrength());
+    }
+    
+    //WEIGHT
+    @Test
+    public final void getWeight_SingleCase() {
+    	assertEquals(5, weaponLegal.getWeight(), 0.25);
+    	assertEquals(5, purseLegal.getWeight(), 0.25);
+    	assertEquals(5, backpackLegal.getWeight(), 0.25);
+    }
+    
+    //EQUIPMENT
+    @Test
+    public final void equipInAnchor_LegalCase() {
+    	Weapon caladbolg = new Weapon(5,5,5, null);
+    	monsterLegal.equipInAnchor(caladbolg, 3);
+        assertTrue(monsterLegal.hasAsEquipment(caladbolg));
+    }
+    
+    @Test
+    public final void equipInAnchor_IllegalArgument() {
+    	Weapon caladbolg = new Weapon(5,5,5, null);
+    	Weapon HMSdivinity = new Weapon(5,5,5, null);
+    	monsterLegal.equipInAnchor(caladbolg, 3);
+    	assertThrows(IllegalArgumentException.class, () -> monsterLegal.equipInAnchor(HMSdivinity, 3));
+    }
+    
+    @Test
+    public final void equipInAnchor_IllegalIndex() {
+    	Weapon caladbolg = new Weapon(5,5,5, null);
+    	assertThrows(IndexOutOfBoundsException.class, () -> monsterLegal.equipInAnchor(caladbolg, -4));
+    }
+    
+    @Test
+    public final void unequip_LegalCase() {
+    	Weapon caladbolg = new Weapon(5,5,5, null);
+    	monsterLegal.equipInAnchor(caladbolg, 3);
+    	monsterLegal.unequip(caladbolg);
+    	assertFalse(monsterLegal.hasAsEquipment(caladbolg));
+    }
+    
+    @Test
+    public final void unequip_IllegalCase() {
+    	Weapon caladbolg = new Weapon(5,5,5, null);
+    	monsterLegal.equipInAnchor(caladbolg, 3);
+    	monsterLegal.unequip(caladbolg);
+    	assertFalse(monsterLegal.hasAsEquipment(caladbolg));
+    }
+    
+    @Test
+    public final void swapItem_LegalCase() {
+    	Weapon caladbolg = new Weapon(5,5,5, null);
+    	Weapon HMSdivinity = new Weapon(5,5,5, null);
+    	monsterLegal.equipInAnchor(caladbolg, 3);
+    	monsterLegal.swapItem(caladbolg, HMSdivinity);
+    	assertTrue(monsterLegal.hasAsEquipment(HMSdivinity));
+    	assertFalse(monsterLegal.hasAsEquipment(caladbolg));
+    }
+    
+    @Test
+    public final void calculateEquipmentLoad_SingleCase() {
+    	monsterLegal.calculateEquipmentLoad();
+    	assertEquals(15, monsterLegal.getEquipmentLoad(), 0.25);
+    }
+    
+    @Test
+    public final void calculateEquipmentValue_SingleCase() {
+    	monsterLegal.calculateEquipmentValue();
+    	assertEquals(25, monsterLegal.getEquipmentValue(), 0.25);
+    }
+    
+    @Test
+    public final void obtainItem_LegalCase() {
+    	Weapon caladbolg = new Weapon(5,1,5, null);
+    	monsterLegal.obtainItem(caladbolg);
+    	assertTrue(backpackLegal.hasAsEquipment(caladbolg));
+    }
+    
+    @Test
+    public final void obtainItem_IllegalCase() {
+    	Weapon caladbolg = new Weapon(5,10000,5, null);
+    	assertThrows(IllegalArgumentException.class, () -> monsterLegal.obtainItem(null)); //Null
+    	assertThrows(IllegalArgumentException.class, () -> monsterLegal.obtainItem(caladbolg)); //Too heavy
+    }
+    
+    @Test
+    public final void destroyWeapon_LegalCase() {
+    	Weapon caladbolg = new Weapon(5,1,5, null);
+    	monsterLegal.equipInAnchor(caladbolg, 3);
+    	monsterLegal.destroyWeapon(caladbolg);
+    	assertFalse(monsterLegal.hasAsEquipment(caladbolg));
+    }
+    
+    @Test
+    public final void destroyWeapon_IllegalCase() {
+    	Weapon caladbolg = new Weapon(5,1,5, null);
+    	assertThrows(IllegalArgumentException.class, () -> monsterLegal.destroyWeapon(caladbolg)); //Not in equipment
+    	assertThrows(NullPointerException.class, () -> monsterLegal.destroyWeapon(null));
+    }
+    
 
 }
