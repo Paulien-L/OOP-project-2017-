8dceb00eabf4c9e0e590e325a9e076cf97cd319b
Paulien-L <34313853+Paulien-L@users.noreply.github.com>
2018-01-10 14:19:08 +0100

    Update project_ItemHolder.java

--- project_ItemHolder.java
+++ project_ItemHolder.java
@@ -1,9 +1,31 @@
-
+/**
+ * Interface of item holders to be implemented by all classes that can be the holder of an Item.
+ * 
+ * @author Thomas and Paulien
+ * 
+ * @version 1
+ *
+ */
 public interface ItemHolder {
-
+	
+	/**
+	 * Method to equip an item.
+	 * @param 	item
+	 * 			Item to be equipped.
+	 */
 	void equip(Item item);
-
+	
+	/**
+	 * Checks if the object that implements this interface has the given item as equipment.
+	 * @param 	item
+	 * 			Item to be checked.
+	 */
 	boolean hasAsEquipment(Item item);
-
+	
+	/**
+	 * Checks if the object that implements this interface can obtain the given item.
+	 * @param 	item
+	 * 			Item to be checked.
+	 */
 	boolean canObtain(Item item);
 }
